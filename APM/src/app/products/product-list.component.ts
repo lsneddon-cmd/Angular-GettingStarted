fdeb8e9ee493e578fdd7ff11e26690a98b698837
Lewis Sneddon <>
2021-01-04 11:35:12 +0000

    using http to retrieve products

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -12,7 +12,8 @@ export class ProductListComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  
+  errorMessage: string;
+
   _listFilter: string;
   get listFilter(): string {
     return this._listFilter;
@@ -26,7 +27,7 @@ export class ProductListComponent implements OnInit {
   products: IProduct[];
 
   constructor(private productService: ProductService) {
-
+ 
   }
 
   onRatingClicked(message: string): void {
@@ -43,7 +44,13 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.products = this.productService.getProducts();
-    this.filteredProducts = this.products;
+    this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error: err => this.errorMessage = err
+    });
+  
   }
 }
